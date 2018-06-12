f7deb3cf3772e06de2135ab5537d02bcf0337e35
Mario Ruiz <mruizfont@moneygram.com>
2018-06-11 23:34:11 -0500

    service worker fetch

--- project_phase1/js/sw.js
+++ project_phase1/js/sw.js
@@ -2,17 +2,21 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
+        '/project_phase1/',
+        '/project_phase1/index.html',
+        '/project_phase1/css/style.css',
+        '/project_phase1/js/main.js',
+        '/project_phase1/image-list.js',
+        '/project_phase1/star-wars-logo.jpg',
+        '/project_phase1/gallery/',
+        '/project_phase1/gallery/bountyHunters.jpg',
+        '/project_phase1/gallery/myLittleVader.jpg',
+        '/project_phase1/gallery/snowTroopers.jpg'
       ]);
     })
   );
+});
+
+self.addEventListener('fetch', function(event){
+	console.log(event.request);
 });
