79d0b90a425eadc7d489b180fc26e0646f873b9b
Mario Ruiz <mruizfont@moneygram.com>
2018-06-15 08:12:19 -0500

    breadcrumb js

--- project_phase1/js/restaurant_info.js
+++ project_phase1/js/restaurant_info.js
@@ -203,6 +203,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute("aria-current","page");
   breadcrumb.appendChild(li);
 }
 
