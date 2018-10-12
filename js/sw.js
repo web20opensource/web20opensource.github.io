05ba86f65bb33c5e16b7169ee163fd2537cd1156
Mario Ruiz <mruizfont@moneygram.com>
2018-10-12 09:54:18 -0500

    sw updated

--- js/sw.js
+++ js/sw.js
@@ -86,7 +86,8 @@ self.addEventListener('activate', function(event) {
 });
 
 
-self.addEventListener("fetch", function(event){
+self.addEventListener('fetch', function(event) {
+  debugger;
   console.log(event.request.url);
   event.respondWith(
         caches.match(event.request).then(function(response){
