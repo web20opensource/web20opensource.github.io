aa044580612dac2b42e9cd32760104fd741cca51
Mario Ruiz <mruizfont@moneygram.com>
2018-10-25 17:39:50 -0500

    favorite feature

--- js/main.js
+++ js/main.js
@@ -270,21 +270,18 @@ createRestaurantHTML = (restaurant, indexRestaurant) => {
 
 
   const fav = document.createElement('span');
+  if (restaurant.is_favorite === 'true'){
+    fav.className = "favRest";
+    fav.setAttribute('aria-label','This is one of my favorite restaurants');
+  }
+  else{
+    fav.className = "notFavRest";
+    fav.setAttribute('aria-label','Not one of my favorites');
+  }
 
   fav.addEventListener('click',(e)=>{
     //TODO improve when offline ... Handle the server response and then update the UI. 
     if (DBHelper.network){
-      
-      if (!e.target.className){
-        //first time, not set yet.
-        if (restaurant.is_favorite === 'true'){
-          fav.className = "favRest";
-        }
-        else{
-          fav.className = "notFavRest";
-        }
-      }
-      
       if(e.target.className == 'favRest'){
         e.target.className = 'notFavRest';
         fav.setAttribute('aria-label','Not one of my favorites');
@@ -300,14 +297,12 @@ createRestaurantHTML = (restaurant, indexRestaurant) => {
           {method: 'PUT'}
           );
       }
-      
     }else{
       alert("Try again when You are online!");
     }
-  });
+  }); 
 
   fav.setAttribute('data-src', restaurant.id);
-  fav.click();
 
   li.append(fav);
 
